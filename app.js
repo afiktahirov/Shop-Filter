519efdf378901ec2f9f6645522dbbabd6670acbe
Afik Tahirov <afik.t38@gmail.com>
2023-08-22 01:20:41 +0400

    firs version

---
+++ app.js
@@ -0,0 +1,402 @@
+let products = [
+  {
+    id: 1,
+    image: "https://neal.fun/spend/images/big-mac.jpg",
+    price: 2,
+    name: "Big Mac",
+  },
+  {
+    id: 2,
+    image: "https://neal.fun/spend/images/flip-flops.jpg",
+    price: 3,
+    name: "Flip Flops",
+  },
+  {
+    id: 3,
+    image: "https://neal.fun/spend/images/coca-cola-pack.jpg",
+    price: 5,
+    name: "Coca-Cola Pack",
+  },
+  {
+    id: 4,
+    image: "https://neal.fun/spend/images/movie-ticket.jpg",
+    price: 12,
+    name: "Movie Ticket",
+  },
+  {
+    id: 5,
+    image: "https://neal.fun/spend/images/book.jpg",
+    price: 15,
+    name: "Book",
+  },
+  {
+    id: 6,
+    image: "https://neal.fun/spend/images/lobster-dinner.jpg",
+    price: 45,
+    name: "Lobster Dinner",
+  },
+  {
+    id: 7,
+    image: "https://neal.fun/spend/images/video-game.jpg",
+    price: 60,
+    name: "Video Game",
+  },
+  {
+    id: 8,
+    image: "https://neal.fun/spend/images/amazon-echo.jpg",
+    price: 99,
+    name: "Amazon Echo",
+  },
+  {
+    id: 9,
+    image: "https://neal.fun/spend/images/year-of-netflix.jpg",
+    price: 100,
+    name: "Year of Netflix",
+  },
+  {
+    id: 10,
+    image: "https://neal.fun/spend/images/air-jordans.jpg",
+    price: 125,
+    name: "Air Jordans",
+  },
+  {
+    id: 11,
+    image: "https://neal.fun/spend/images/airpods.jpg",
+    price: 199,
+    name: "Airpods",
+  },
+  {
+    id: 12,
+    image: "https://neal.fun/spend/images/gaming-console.jpg",
+    price: 299,
+    name: "Gaming Console",
+  },
+  {
+    id: 13,
+    image: "https://neal.fun/spend/images/drone.jpg",
+    price: 350,
+    name: "Drone",
+  },
+  {
+    id: 14,
+    image: "https://neal.fun/spend/images/smartphone.jpg",
+    price: 699,
+    name: "Smartphone",
+  },
+  {
+    id: 15,
+    image: "https://neal.fun/spend/images/bike.jpg",
+    price: 800,
+    name: "Bike",
+  },
+  {
+    id: 16,
+    image: "https://neal.fun/spend/images/kitten.jpg",
+    price: 1500,
+    name: "Kitten",
+  },
+  {
+    id: 17,
+    image: "https://neal.fun/spend/images/puppy.jpg",
+    price: 1500,
+    name: "Puppy",
+  },
+  {
+    id: 18,
+    image: "https://neal.fun/spend/images/auto-rickshaw.jpg",
+    price: 2300,
+    name: "Auto Rickshaw",
+  },
+  {
+    id: 19,
+    image: "https://neal.fun/spend/images/horse.jpg",
+    price: 2500,
+    name: "Horse",
+  },
+  {
+    id: 20,
+    image: "https://neal.fun/spend/images/acre-of-farmland.jpg",
+    price: 3000,
+    name: "Acre of Farmland",
+  },
+  {
+    id: 21,
+    image: "https://neal.fun/spend/images/designer-handbag.jpg",
+    price: 5500,
+    name: "Designer Handbag",
+  },
+  {
+    id: 22,
+    image: "https://neal.fun/spend/images/hot-tub.jpg",
+    price: 6000,
+    name: "Hot Tub",
+  },
+  {
+    id: 23,
+    image: "https://neal.fun/spend/images/luxury-wine.jpg",
+    price: 7000,
+    name: "Luxury Wine",
+  },
+  {
+    id: 24,
+    image: "https://neal.fun/spend/images/diamond-ring.jpg",
+    price: 10000,
+    name: "Diamond Ring",
+  },
+  {
+    id: 25,
+    image: "https://neal.fun/spend/images/jet-ski.jpg",
+    price: 12000,
+    name: "Jet Ski",
+  },
+  {
+    id: 26,
+    image: "https://neal.fun/spend/images/rolex.jpg",
+    price: 15000,
+    name: "Rolex",
+  },
+  {
+    id: 27,
+    image: "https://neal.fun/spend/images/ford-f-150.jpg",
+    price: 30000,
+    name: "Ford F-150",
+  },
+  {
+    id: 28,
+    image: "https://neal.fun/spend/images/tesla.jpg",
+    price: 75000,
+    name: "Tesla",
+  },
+  {
+    id: 29,
+    image: "https://neal.fun/spend/images/monster-truck.jpg",
+    price: 150000,
+    name: "Monster Truck",
+  },
+  {
+    id: 30,
+    image: "https://neal.fun/spend/images/ferrari.jpg",
+    price: 250000,
+    name: "Ferrari",
+  },
+  {
+    id: 31,
+    image: "https://neal.fun/spend/images/single-family-home.jpg",
+    price: 300000,
+    name: "Single Family Home",
+  },
+  {
+    id: 32,
+    image: "https://neal.fun/spend/images/gold-bar.jpg",
+    price: 700000,
+    name: "Gold Bar",
+  },
+  {
+    id: 33,
+    image: "https://neal.fun/spend/images/mcdonalds-franchise.jpg",
+    price: 1500000,
+    name: "McDonalds Franchise",
+  },
+  {
+    id: 34,
+    image: "https://neal.fun/spend/images/superbowl-ad.jpg",
+    price: 5250000,
+    name: "Superbowl Ad",
+  },
+  {
+    id: 35,
+    image: "https://neal.fun/spend/images/yacht.jpg",
+    price: 7500000,
+    name: "Yacht",
+  },
+  {
+    id: 36,
+    image: "https://neal.fun/spend/images/m1-abrams.jpg",
+    price: 8000000,
+    name: "M1 Abrams",
+  },
+  {
+    id: 37,
+    image: "https://neal.fun/spend/images/formula-1-car.jpg",
+    price: 15000000,
+    name: "Formula 1 Car",
+  },
+  {
+    id: 38,
+    image: "https://neal.fun/spend/images/apache-helicopter.jpg",
+    price: 31000000,
+    name: "Apache Helicopter",
+  },
+  {
+    id: 39,
+    image: "https://neal.fun/spend/images/mansion.jpg",
+    price: 45000000,
+    name: "Mansion",
+  },
+  {
+    id: 40,
+    image: "https://neal.fun/spend/images/make-a-movie.jpg",
+    price: 100000000,
+    name: "Make a Movie",
+  },
+  {
+    id: 41,
+    image: "https://neal.fun/spend/images/boeing-747.jpg",
+    price: 148000000,
+    name: "Boeing 747",
+  },
+  {
+    id: 42,
+    image: "https://neal.fun/spend/images/mona-lisa.jpg",
+    price: 780000000,
+    name: "Mona Lisa",
+  },
+  {
+    id: 43,
+    image: "https://neal.fun/spend/images/skyscraper.jpg",
+    price: 850000000,
+    name: "Skyscraper",
+  },
+  {
+    id: 44,
+    image: "https://neal.fun/spend/images/cruise-ship.jpg",
+    price: 930000000,
+    name: "Cruise Ship",
+  },
+  {
+    id: 45,
+    image: "https://neal.fun/spend/images/nba-team.jpg",
+    price: 2120000000,
+    name: "NBA Team",
+  },
+];
+let tempProducts = products;
+let basket = JSON.parse(localStorage.getItem("basket")) || [];
+const section = document.querySelector(".products");
+const header = document.querySelector("header");
+const reciept = document.querySelector("#reciept");
+const search__input = document.getElementById("search__input");
+const balance = 450000;
+let total = 0;
+search__input.addEventListener("input", () => {
+  products = tempProducts.filter((a) =>
+    a.name.toLowerCase().startsWith(search__input.value.toLowerCase())
+  );
+  showProducts();
+});
+const calculateTotal = () => {
+  total = basket.reduce(
+    (acc, item) =>
+      acc + products.find((t) => t.id === item.id).price * item.count,
+    0
+  );
+  header.textContent = `$${balance - total}`;
+};
+
+const buyProduct = (id) => {
+  if (balance - total < products.find((t) => t.id === id).price) {
+    return;
+  }
+  const item = basket.find((a) => a.id === id);
+  if (item) {
+    item.count++;
+  } else {
+    basket.push({
+      id: id,
+      count: 1,
+    });
+  }
+  localStorage.setItem("basket", JSON.stringify(basket));
+  showProducts();
+};
+
+const sellProduct = (id) => {
+  const item = basket.find((a) => a.id === id);
+  if (!item) {
+    return;
+  }
+  item.count--;
+  if (!item.count) {
+    basket = basket.filter((a) => a.id !== id);
+  }
+  localStorage.setItem("basket", JSON.stringify(basket));
+  showProducts();
+};
+const showProducts = () => {
+  calculateTotal();
+  showReciept();
+  section.textContent = "";
+  if (!products.length) {
+    section.textContent = "Axtardığınız məhsul tapılmadı !";
+    return;
+  }
+  products.map((product, y) => {
+    const productInBasket = basket.find((a) => a.id === product.id);
+    const productDiv = document.createElement("div");
+    productDiv.classList.add("product");
+    const image = document.createElement("img");
+    image.setAttribute("src", product.image);
+    const name = document.createElement("h3");
+    name.textContent = product.name;
+    const price = document.createElement("h3");
+    price.textContent = `$${product.price}`;
+    const operationsDiv = document.createElement("div");
+    operationsDiv.classList.add("operations");
+    const sellButton = document.createElement("button");
+    sellButton.textContent = "Sell";
+    sellButton.classList.add("sell");
+    sellButton.disabled = !productInBasket;
+    sellButton.addEventListener("click", () => sellProduct(product.id));
+    const elementCount = document.createElement("h3");
+    elementCount.textContent = productInBasket ? productInBasket.count : 0;
+    const buyButton = document.createElement("button");
+    buyButton.textContent = "Buy";
+    buyButton.disabled = balance - total < product.price;
+    buyButton.classList.add("buy");
+    buyButton.addEventListener("click", () => buyProduct(product.id));
+    operationsDiv.append(sellButton, elementCount, buyButton);
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "Delete";
+    deleteButton.addEventListener("click", () => {
+      basket = basket.filter((u) => u.id !== product.id);
+      products.splice(y, 1);
+      showProducts();
+    });
+    productDiv.append(image, name, price, operationsDiv, deleteButton);
+    section.append(productDiv);
+  });
+};
+
+const showReciept = () => {
+  reciept.textContent = "";
+  basket.map((a) => {
+    const product = products.find((t) => t.id === a.id);
+    const li = document.createElement("li");
+    li.textContent = `${product.name} x ${a.count} = $${
+      a.count * product.price
+    }`;
+    reciept.append(li);
+  });
+  const totalLi = document.createElement("li");
+  totalLi.textContent = `Total : $${total}`;
+  reciept.append(totalLi);
+};
+
+showProducts();
+
+//https://neal.fun/life-stats/
+
+//Reciept functionallity
+//Search functionallity
+//Remove Product
+
+//LocalStorage -> Arashdir
+//parentElement
+//nextElementSibling
+//previousElementSibling
+//children
+//closest
+
+//cloneNode
+//isNaN()
+//isEqualNode()
